2bded5eb5a24c68e2e48e036784bb0651db25ae5
@m.jilevskiy <m.jilevskiy@aerodisk.ru>
2024-09-28 16:00:34 +0300

    ревью правки

--- fsd/features/auth/ui/LoginForm.tsx
+++ fsd/features/auth/ui/LoginForm.tsx
@@ -1,23 +1,23 @@
-'use client';
-import React from 'react';
-import { Button } from 'antd';
-import { useRouter } from 'next/navigation'; // импортируем хук useRouter
-import l from './LoginForm.module.scss'
+"use client";
+import React from "react";
+import { Button } from "antd";
+import { useRouter } from "next/navigation";
+import l from "./LoginForm.module.scss";
 
 const LoginForm = () => {
-	const router = useRouter(); // создаем инстанс useRouter
+  const router = useRouter();
 
-	const handleLogin = () => {
-		router.push('/profile'); // выполняем редирект на страницу profile
-	};
+  const handleLogin = () => {
+    router.push("/profile");
+  };
 
-	return (
-		<div className={l.login_form}>
-			<Button type='primary' onClick={handleLogin}>
-				login via UNI
-			</Button>
-		</div>
-	);
+  return (
+    <div className={l.login_form}>
+      <Button type="primary" onClick={handleLogin}>
+        login via UNI
+      </Button>
+    </div>
+  );
 };
 
 export default LoginForm;
