25b19408d813f4b17211db8aeb0cfa98519df144
@m.jilevskiy <m.jilevskiy@aerodisk.ru>
2024-09-29 22:17:42 +0300

    нав бар тестовый

--- fsd/pages/Login.tsx
+++ fsd/pages/Login.tsx
@@ -4,7 +4,7 @@ import LoginForm from '../features/auth/ui/LoginForm';
 
 const Login = () => {
 	return (
-		<div className='w-[150px] bg-red-500'>
+		<div>
 			<h1>Login</h1>
 			<LoginForm />
 		</div>
