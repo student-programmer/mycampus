a9b502db00c4e75439c13b98a0d34fb881e10e79
stuedent-programmer <tazer16179@gmail.com>
2024-11-16 22:55:24 +0300

    страница чатов

--- fsd/widgets/chat/ChatDetail.tsx
+++ fsd/widgets/chat/ChatDetail.tsx
@@ -38,42 +38,45 @@ const ChatDetail = () => {
   };
 
   return (
-    <div className={style.wrapperDetail}>
-      <div className={style.goBackContainer}>
-        <LeftOutlined className={style.iconGoBack} onClick={() => router.back()} />
-        <h2 className="h2">User: {chatNameDecode}</h2>
-      </div>
-      <List
-        className={style.listContainer}
-        dataSource={messages}
-        renderItem={(item) => (
-          <List.Item>
-            <List.Item.Meta
-              className=""
-              avatar={<Avatar src={item.avatar} />}
-              title={item.sender}
-              description={item.content}
-            />
-          </List.Item>
-        )}
-      />
-      <div className={style.inputContainer}>
-        <Input
-          className="inputBase"
-          placeholder="Input message..."
-          value={inputValue}
-          onChange={(e) => setInputValue(e.target.value)}
-          onPressEnter={handleSendMessage}
-        />
-        <Button
-          type="primary"
-          className="defaultButton"
-          icon={<SendOutlined />}
-          onClick={handleSendMessage}
-        />
-      </div>
-    </div>
-  );
+		<div className={style.wrapperDetail}>
+			<div className={style.goBackContainer}>
+				<LeftOutlined
+					className={style.iconGoBack}
+					onClick={() => router.back()}
+				/>
+				<h2 className='h2'>User: {chatNameDecode}</h2>
+			</div>
+			<List
+				className={style.listContainer}
+				dataSource={messages}
+				renderItem={item => (
+					<List.Item>
+						<List.Item.Meta
+							className=''
+							avatar={<Avatar src={item.avatar} />}
+							title={item.sender}
+							description={item.content}
+						/>
+					</List.Item>
+				)}
+			/>
+			<div className={style.inputContainer}>
+				<Input
+					className={style.inputs}
+					placeholder='Input message...'
+					value={inputValue}
+					onChange={e => setInputValue(e.target.value)}
+					onPressEnter={handleSendMessage}
+				/>
+				<Button
+					type='primary'
+					className='defaultButton'
+					icon={<SendOutlined />}
+					onClick={handleSendMessage}
+				/>
+			</div>
+		</div>
+	);
 };
 
 export default ChatDetail;
