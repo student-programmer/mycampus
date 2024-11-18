bed72bb3366c498d9e5e877152249641c486b04d
@m.jilevskiy <m.jilevskiy@aerodisk.ru>
2024-11-19 01:21:40 +0300

    доделал events

--- fsd/widgets/events/CardInfoAndDetail.tsx
+++ fsd/widgets/events/CardInfoAndDetail.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import AdressIcon from "@/public/adressIcon";
 import { EventCardProps } from "./EventsCard";
 import style from "./ui/eventDesc.module.scss";
@@ -7,12 +7,13 @@ import dayjs from "dayjs";
 import { useState } from "react";
 import Image from "next/image";
 import closeIcon from "../../../public/iconClose.svg";
-
+import { Overview } from "@/fsd/shared/ui/Overview/Overview";
+import { Contacts } from "@/fsd/shared/ui/Contacts/Contact";
 
 const CardInfoAndDetail = ({ event }: EventCardProps) => {
   const dateInit = dayjs(event.date);
   const timeInit = dayjs().hour(20).minute(0).second(0);
-const [openDetail, setOpenDetail] = useState(false);
+  const [openDetail, setOpenDetail] = useState(false);
   return (
     <div className={style.eventsCardWrapper}>
       <div className={style.headerWrapper}>
@@ -49,6 +50,20 @@ const [openDetail, setOpenDetail] = useState(false);
         >
           Details
         </Button>
+      )}
+      {openDetail && (
+        <>
+          <Overview text={event.description} />
+          <div className={style.contactsWrapper}>
+            <h2 className={style.contactsH2}>Contacts:</h2>
+            <Contacts
+              phoneNumber={event.contacts.phone}
+              instagram={event.contacts.inst}
+              maps={event.contacts.maps}
+              website={event.contacts.web}
+            />
+          </div>
+        </>
       )}
     </div>
   );
