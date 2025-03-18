d2dd004ffc06ff74c71fb8717180a15ec2fcb3d1
studentprogrammer <tazer16179@gmail.com>
2025-03-18 18:01:59 +0300

    изменил запросы с http на https

--- plugins/axios.ts
+++ plugins/axios.ts
@@ -3,7 +3,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
 	timeout: 10000,
-	baseURL: 'http://45.9.43.192:3000',
+	baseURL: 'https://45.9.43.192:3000',
 	headers: {
 		'Content-Type': 'application/json',
 		accept: 'application/json',
